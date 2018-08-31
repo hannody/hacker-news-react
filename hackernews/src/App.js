b48c72d1dc879a081a3aba6f6336ae783b449bb4
hannody <aljaboori@gmail.com>
2018-08-31 21:03:43 +0800

    [

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -25,19 +25,19 @@ const list = [
       return (
         <div className="App">
           {list.map(function(item) {
-return (
-<div key={item.objectID}>
-<span>
-<a href={item.url}>{item.title}</a>
-</span>
-<span>{item.author}</span>
-<span>{item.num_comments}</span>
-<span>{item.points}</span>
-</div>
-);
-})}
-    </div>
-    );
+              return (
+                  <div key={item.objectID}>
+                      <span>
+                        <a href={item.url}>{item.title}</a>
+                      </span>
+                      <span>{item.author}</span>
+                      <span>{item.num_comments}</span>
+                      <span>{item.points}</span>
+                  </div>
+              );
+              })}
+                  </div>
+                  );
     }
     }
 
